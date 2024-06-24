a9dda2ccddbe6cb809669eb8bc42d4c02de2497a
Aliny Melquiades <alinymelquiadesdesouza@gmail.com>
2024-06-24 16:43:16 -0300

    Update exercicio3.java

--- src/main/java/pacote/exercicio3.java
+++ src/main/java/pacote/exercicio3.java
@@ -4,31 +4,36 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-// Crie um método para inserir Categorias. Se preferir crie um objeto, com as
-//características encontradas na tabela, e insira esse objeto no banco.
+//3- Crie um método para inserir Gêneros. Se preferir crie um objeto, com as
+//características encontradas na tabela, e insira esse objeto no banco
 
-public class exercicio2 {
+public class exercicio3 {
 
     private final String url = "jdbc:postgresql://localhost:5432/postgres";
 
+    public exercicio3(String rua, String cidade, String estado, String cep) {
+    }
 
-    public void inserirCategoria(exercicio2 categoria) {
-        String sql = "INSERT INTO categoria(nome) VALUES(?)";
+    // Método para inserir um Gênero no banco de dados
+    public void inserirGenero(exercicio3 genero) {
+        String sql = "INSERT INTO genero(nome) VALUES(?)";
 
         try (Connection conn = DriverManager.getConnection(url);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            pstmt.setString(1, categoria.getNome());
+            // Configurar os parâmetros do PreparedStatement
+            pstmt.setString(1, genero.getNome());
+
+            // Executar a inserção
             pstmt.executeUpdate();
-            System.out.println("Categoria inserida com sucesso.");
+            System.out.println("Gênero inserido com sucesso.");
 
         } catch (SQLException e) {
-            System.out.println("Erro ao inserir categoria: " + e.getMessage());
+            System.out.println("Erro ao inserir gênero: " + e.getMessage());
         }
     }
 
     private String getNome() {
         return "";
-    }
-}
+    } }
 
